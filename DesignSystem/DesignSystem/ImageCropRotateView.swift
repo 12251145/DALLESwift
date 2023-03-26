56e178be23967bfdbf62053b5a0919a90b9bf5d8
1145 <team_halves.0h@icloud.com>
2023-03-26 10:16:59 +0900

    Image 자르기 View 줌도 가능하게 구현

--- DesignSystem/DesignSystem/ImageCropRotateView.swift
+++ DesignSystem/DesignSystem/ImageCropRotateView.swift
@@ -11,12 +11,14 @@ import UIKit
 public final class ImageCropRotateView: UIView {
     private let scrollView: UIScrollView
     private let imageView: UIImageView
+    private let imagePaddingView: UIView
     private let cropOverlay: UIView
     
     public var image: UIImage? {
         didSet {
             imageView.image = image
             imageView.sizeToFit()
+            imagePaddingView.frame = imageView.frame
 
             setNeedsLayout()
         }
@@ -27,6 +29,7 @@ public final class ImageCropRotateView: UIView {
     public init(cropSize: CGSize) {
         self.scrollView = UIScrollView()
         self.imageView = UIImageView()
+        self.imagePaddingView = UIView()
         self.cropOverlay = UIView()
         self.cropSize = cropSize
         
@@ -57,21 +60,22 @@ public final class ImageCropRotateView: UIView {
         
         let topMargin = cropOverlay.frame.minY
         let leftMargin = cropOverlay.frame.minX
-        imageView.pin.top(topMargin).left(leftMargin)
+        imagePaddingView.pin.top(topMargin).left(leftMargin)
         
-        let contentWidth = imageView.bounds.size.width + (scrollView.bounds.size.width - cropSize.width)
-        let contentHeight = imageView.bounds.size.height + (scrollView.bounds.size.height - cropSize.height)
+        let contentWidth = imagePaddingView.bounds.size.width + (scrollView.bounds.size.width - cropSize.width)
+        let contentHeight = imagePaddingView.bounds.size.height + (scrollView.bounds.size.height - cropSize.height)
         scrollView.contentSize = .init(width: contentWidth, height: contentHeight)
         
-        let contentOffsetX = (imageView.bounds.size.width / 2) - ((self.bounds.size.width / 2) - cropOverlay.frame.minX)
-        let contentOffsetY = (imageView.bounds.size.height / 2) - ((self.bounds.size.height / 2) - cropOverlay.frame.minY)
+        let contentOffsetX = (imagePaddingView.bounds.size.width / 2) - ((self.bounds.size.width / 2) - cropOverlay.frame.minX)
+        let contentOffsetY = (imagePaddingView.bounds.size.height / 2) - ((self.bounds.size.height / 2) - cropOverlay.frame.minY)
         scrollView.contentOffset = .init(x: contentOffsetX, y: contentOffsetY)
     }
     
     private func setUpScrollView() {
         scrollView.delegate = self
-        
-        // TODO: PinchGesture도 가능하게 만들어야 함
+                
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 3.0
         
         scrollView.showsVerticalScrollIndicator = false
         scrollView.showsHorizontalScrollIndicator = false
@@ -80,7 +84,8 @@ public final class ImageCropRotateView: UIView {
     }
     
     private func setupImageView() {
-        scrollView.addSubview(imageView)
+        imagePaddingView.addSubview(imageView)
+        scrollView.addSubview(imagePaddingView)
     }
 
     private func setUpCropOverlay() {
@@ -94,12 +99,32 @@ public final class ImageCropRotateView: UIView {
 // MARK: - UIScrollViewDelegate
 extension ImageCropRotateView: UIScrollViewDelegate {
     public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return imageView
+        let paddingHeight = scrollView.bounds.size.height - cropSize.height
+        let paddingWidth = scrollView.bounds.size.width - cropSize.width
+        let origin = imagePaddingView.frame.origin
+        var tmpSize = imagePaddingView.frame.size
+        tmpSize.width += paddingWidth
+        tmpSize.height += paddingHeight
+        imagePaddingView.frame = .init(origin: origin, size: tmpSize)
+        return imagePaddingView
     }
 }
 
 // MARK: - UIGestureRecognizerDelegate
-
 extension ImageCropRotateView: UIGestureRecognizerDelegate {
-    
+
+    public func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        
+        guard let view else { return }
+        
+        let currentImageWidth = imageView.frame.size.width * scale
+        let currentImageHeight = imageView.frame.size.height * scale
+        
+        imagePaddingView.frame = .init(origin: view.frame.origin, size: .init(width: currentImageWidth, height: currentImageHeight))
+        
+        let contentWidth = imagePaddingView.frame.size.width + (scrollView.bounds.size.width - cropSize.width)
+        let contentHeight = imagePaddingView.frame.size.height + (scrollView.bounds.size.height - cropSize.height)
+        
+        scrollView.contentSize = .init(width: contentWidth, height: contentHeight)        
+    }
 }
