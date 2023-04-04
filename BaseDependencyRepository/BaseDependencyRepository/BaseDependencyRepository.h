e6ef9c0c1955401ed828471fd71ceb1eec863aa0
1145 <team_halves.0h@icloud.com>
2023-04-04 23:30:48 +0900

    의존성 정리, 폴더 정리

---
+++ BaseDependencyRepository/BaseDependencyRepository/BaseDependencyRepository.h
@@ -0,0 +1,18 @@
+//
+//  BaseDependencyRepository.h
+//  BaseDependencyRepository
+//
+//  Created by Hoen on 2023/04/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseDependencyRepository.
+FOUNDATION_EXPORT double BaseDependencyRepositoryVersionNumber;
+
+//! Project version string for BaseDependencyRepository.
+FOUNDATION_EXPORT const unsigned char BaseDependencyRepositoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseDependencyRepository/PublicHeader.h>
+
+
---
+++ FeatureDALLE/FeatureDALLE/FeatureDALLE.h
@@ -0,0 +1,18 @@
+//
+//  FeatureDALLE.h
+//  FeatureDALLE
+//
+//  Created by Hoen on 2023/04/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeatureDALLE.
+FOUNDATION_EXPORT double FeatureDALLEVersionNumber;
+
+//! Project version string for FeatureDALLE.
+FOUNDATION_EXPORT const unsigned char FeatureDALLEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeatureDALLE/PublicHeader.h>
+
+
